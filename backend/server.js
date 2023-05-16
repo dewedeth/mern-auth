4af5e9481117281699ec628228441774bc148a4f
dewedeth <yousefbacarro@icloud.com>
2023-05-15 21:26:11 -0400

    first commit

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/userRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+connectDB();
+
+app.use("/api/users", userRoutes);
+
+app.get("/", (req, res) => res.send("server is ready boss"));
+
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
